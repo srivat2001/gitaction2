de1a0ad1a3e5712333d86cdf687fb2db8e99b6f2
Srivathsan G <77399329+srivat2001@users.noreply.github.com>
2025-06-16 09:32:49 +0530

    Changed action to include tests and added aching:

--- app.js
+++ app.js
@@ -1,6 +1,16 @@
+const axios = require('axios');
+
 function add(a, b) {
     return a + b;
-  }
-  
-  // Export the function for testing
-  module.exports = { add };
+}
+
+async function fetchData() {
+    const res = await axios.get('https://jsonplaceholder.typicode.com/posts/1');
+    console.log(res.data);
+}
+
+module.exports = { add, fetchData };
+
+if (require.main === module) {
+    fetchData();
+}
